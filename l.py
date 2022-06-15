975929ea9ebfd03e49560a70037c3f6b3fc242f1
hex-313 <78981792+hex-313@users.noreply.github.com>
2022-06-16 04:46:43 +0530

    Create l.py

---
+++ l.py
@@ -0,0 +1,8 @@
+from urllib.parse import quote
+import sys
+
+print ('http://localhost:8000/%s' % (quote(sys.argv[1])))
+from urllib.parse import quote
+import sys
+
+print ('http://localhost:8000/%s' % (quote(sys.argv[1])))
