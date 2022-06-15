b25d022714fc735cf68f4ac1440cd861a8aa06e0
hex-313 <78981792+hex-313@users.noreply.github.com>
2022-06-16 04:48:26 +0530

    Update l.py

--- l.py
+++ l.py
@@ -2,7 +2,3 @@ from urllib.parse import quote
 import sys
 
 print ('http://localhost:8000/%s' % (quote(sys.argv[1])))
-from urllib.parse import quote
-import sys
-
-print ('http://localhost:8000/%s' % (quote(sys.argv[1])))
