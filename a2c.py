083e542d2903428c638510bdba48ce68afd3e6e4
hex-313 <78981792+hex-313@users.noreply.github.com>
2022-06-16 06:48:03 +0530

    Update a2c.py

--- a2c.py
+++ a2c.py
@@ -6,7 +6,7 @@ from urllib.parse import unquote_plus
 link = sys.argv[1]
 
 try:
-  run(f'extra-api', -x 10 "{link}", shell=True)
+  run(f'extra-api', '-x', '10', "{link}", shell=True)
 
 
 print ('%s' % unquote_plus(link).rsplit('/', 1)[-1])
