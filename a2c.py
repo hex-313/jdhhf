ad710370afbb94da1591c28b675be21eea5a518c
hex-313 <78981792+hex-313@users.noreply.github.com>
2022-06-16 06:40:39 +0530

    Create a2c.py

---
+++ a2c.py
@@ -0,0 +1,12 @@
+import re
+import sys
+from subprocess import run
+from urllib.parse import unquote_plus
+
+link = sys.argv[1]
+
+try:
+  run(f'extra-api' -x 10 "{link}", shell=True)
+
+
+print ('%s' % unquote_plus(link).rsplit('/', 1)[-1])
